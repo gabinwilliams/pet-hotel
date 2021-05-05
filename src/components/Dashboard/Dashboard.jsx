6a6b0df4bb244ff5f591075756d575b8c09c05bc
gabinwilliams <scoutsnknives@gmail.com>
2021-05-05 15:01:19 -0500

    added an input for Dashboard

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Dashboard.css';
+import DashboardInput from '../DashboardInput/DashboardInput';
+
+
+
+const Dashboard = () => {
+
+
+
+  return (
+    <div>
+      <h2 className="dashboardTitle">Add Owner</h2>
+      <DashboardInput />
+    </div>
+  )
+}
+
+export default Dashboard
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -7,6 +7,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Dashboard from '../Dashboard/Dashboard';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -83,7 +84,7 @@ export default function FullWidthTabs() {
         onChangeIndex={handleChangeIndex}
       >
         <TabPanel value={value} index={0} dir={theme.direction}>
-          Item One
+          <Dashboard />
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}>
           Item Two
