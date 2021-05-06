a3ac85ff726f6e713c1c572cd420133f2675be6f
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-05-06 09:07:37 -0500

    first saga and reducer

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Dashboard.css';
 import DashboardInput from '../DashboardInput/DashboardInput';
 import DashboardTable from '../DashboardTable/DashboardTable';
+import {useDispatch} from 'react-redux';
 
 
 
 const Dashboard = () => {
 
+  const dispatch = useDispatch();
 
+  useEffect(()=>{
+    dispatch({type:'FETCH_OWNER'});
+  }, []);
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -6,19 +6,46 @@ import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 import { applyMiddleware } from "redux";
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put, take} from 'redux-saga/effects';
+import axios from 'axios';
 
+
+function* rootSaga(){
+  yield takeEvery('FETCH_OWNER', fetchOwners);
+}
 // questions provided on each page
+function* fetchOwners(){
+  try {
+    const owners = yield axios.get('/api/owners')
+    console.log ('get owners', owners)
+    yield put ({type: 'SET_OWNERS', payload: owners.data})
+  }
+  catch{
+    console.log('get owners error')
+  }
+}
 
+const sagaMiddleware = createSagaMiddleware();
 
 // stores the values selected on feedback pages and is the obj we send in POST to the server
-
+const owners = (state= [], action) =>{
+  switch(action.type){
+    case 'SET_OWNERS':
+      return action.payload
+    default:
+      return state;
+  }
+}
 
 const store = createStore(
   combineReducers({
-  
+    owners,
   }),
-  applyMiddleware(logger)
+  applyMiddleware(sagaMiddleware, logger)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   // wrap app in provider to allow for redux logger in console log
